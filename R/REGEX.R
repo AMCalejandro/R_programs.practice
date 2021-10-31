cbf8bc5c93ad392411e739835e9b4c855c322941
AMCalejandro <alejandro.carrasco.20@ucl.ac.uk>
2021-10-31 14:02:17 +0000

    Adding regex functions using repetition approaches. Adding the testing on the markdown too

--- R/REGEX.R
+++ R/REGEX.R
@@ -31,3 +31,57 @@ ingIse_ending = function(x = NULL) {
   str_subset(x, pattern = "(ing|ise)$")
 }
 
+
+#' consonant_starter
+#'
+#' Given a string or list of strings, it gets those words starting with three consonants 
+#' strings of length equal to three are excluded
+#' 
+#' @param x The string/strings on a vector
+#'
+#' @return the matching words
+#'
+#' @examples
+consonant_starter = function(x = NULL) {
+  library(stringr)
+  str_subset(x, pattern = "^[^aeiou\\d\\s]{3}.+")
+}
+
+
+#' vowels_finder
+#'
+#' Given a string or list of strings, it gets those words starting with three consonants 
+#' strings of length equal to three are excluded
+#' 
+#' @param x The string/strings on a vector
+#'
+#' @return the matching words in either upper or lower case format.
+#'
+#' @examples
+#' 
+vowels_finder = function(x = NULL, capital = "lower") {
+  library(stringr)
+  x = str_to_lower(x)
+  if (capital == "lower"){
+  str_subset(x, pattern = "[aeiou]{3}")
+  }
+  else {str_to_upper(str_subset(x, pattern = "[aeiou]{3}"))
+  }
+}
+
+#' vc_pairs
+#'
+#' Given a string or list of strings, it gets those words that have two or more vowel-consonant pairs in a row
+#' 
+#' @param x The string/strings on a vector
+#'
+#' @return the matching words in either upper or lower case format.
+#'
+#' @examples
+#' 
+vc_finder = function(x = NULL) {
+  library(stringr)
+  x = str_to_lower(x)
+    str_subset(x, pattern = "([aeiou][^aeiou]){2,}")
+}
+
